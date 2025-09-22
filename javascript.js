3f8ff8ca61e76e529a0b18518b7d8cd3043c09ec
ajcur <173679835+ajcur@users.noreply.github.com>
2025-09-22 10:22:05 +0200

    Refactor game logic, add function to determine win state

--- javascript.js
+++ javascript.js
@@ -4,12 +4,36 @@ gameboard = (function() {
         return state;
     }
     function setMark(position, mark) {
-        state[position] = mark;
+        if (position < state.length && position >= 0) {
+            if (state[position] == "") {
+                state[position] = mark;
+            } else {
+                alert ('Not an empty position');
+            }
+        } else {
+            alert('Not a valid gameboard position');
+            return;
+        }
     }
-    return {getState, setMark}
+    function determineWinState(mark) {
+        let winState = false;
+        let i = 0;
+        while (!winState && i <= 8) {
+            if (
+                ((i == 0 || 3 || 6) && (state[i] && state[i+1] && state[i+2] == mark)) ||
+                ((i < 3) && (state[i] && state[i+3] && state[i+6] == mark)) ||
+                ((i == 0) && (state[i] && state[i+4] && state[i+8] == mark)) ||
+                ((i == 2) && (state[i] && state[i+2] && state[i+4] == mark))
+            ){
+                winState = true;
+            } else i++;
+        }
+        return winState;
+        }
+    return {getState, setMark, determineWinState}
 })();
 
-function createPlayer(name, mark) {
+/* function createPlayer(name, mark) {
     let player = {
         name,
         mark,
@@ -18,12 +42,25 @@ function createPlayer(name, mark) {
         }
     }
     return player;
-};
+}; */
 
-alex = createPlayer('alex', 'o');
-jamie = createPlayer('jamie', 'x');
+addPlayers = (function () {
+    let players = [];
+    function createPlayer(name, mark) {
+        let player = {
+            name,
+            mark,
+            makeMark: function(position) {
+                gameboard.setMark(position, this.mark);
+            }
+        }
+        players.push(player);
+        return player;
+    };
+    return {players, createPlayer};
+})();
 
-playTurn = (function(players) {
+createTurn = function(players) {
     let turnIndex = 0;
     let currentPlayer = players[turnIndex];
     function getCurrentPlayer() {
@@ -39,13 +76,47 @@ playTurn = (function(players) {
         currentPlayer.makeMark(position);
     }
     return {getCurrentPlayer, advanceTurn, makeMark};
-})([alex, jamie]);
+};
 
-playTurn.makeMark(3);
+playRound = function(players) {
+    playTurn = createTurn(players);
+    
+    position = prompt(`Position for ${playTurn.getCurrentPlayer().name}?`)
+    playTurn.makeMark(position);
+
+    if (gameboard.getState()[position] != playTurn.getCurrentPlayer().mark) {
+        return;
+    }
+    winState = gameboard.determineWinState(playTurn.getCurrentPlayer().mark);
+
+    if (winState) {
+        let winner = playTurn.getCurrentPlayer().name;
+        alert(`Congratulations! ${winner} won the game!`)
+        return;
+    } else playTurn.advanceTurn();
+    
+    return {winState, winner}
+}
+
+playGame = function() {
+    alex = addPlayers.createPlayer('alex', 'o');
+    jamie = addPlayers.createPlayer('jamie', 'x');
+
+    playTurn = createTurn(addPlayers.players);
+
+    playTurn.makeMark(prompt(`Position for ${playTurn.getCurrentPlayer().name}?`));
+    playTurn.advanceTurn();
+    playTurn.makeMark(prompt(`Position for ${playTurn.getCurrentPlayer().name}?`));
+    console.log(gameboard.getState());
+}
+
+playGame();
+
+/* playTurn.makeMark(prompt(`Position for ${playTurn.getCurrentPlayer().name}?`));
 playTurn.advanceTurn();
-playTurn.makeMark(0);
+playTurn.makeMark(prompt(`Position for ${playTurn.getCurrentPlayer().name}?`)); */
 
-console.log(gameboard.getState());
+/* console.log(gameboard.getState()); */
 
 /* Next steps:
 - Take input of position from player
